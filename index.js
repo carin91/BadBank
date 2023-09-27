63415474a1fe13fe40de309b6fd7b946c8f64b59
Camilo Rincon <123512032+carin91@users.noreply.github.com>
2023-09-27 04:50:02 -0500

    test 11 including consts

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const Route       = ReactRouterDOM.Route;
+const Link        = ReactRouterDOM.Link;
+const HashRouter  = ReactRouterDOM.HashRouter;
+// create context
+const UserContext = React.createContext(null);
+
 function Spa() {
   return (
-    {/*<HashRouter>*/}
-    <div className="container" style={{padding: "20px"}}>
-      <NavBar>
-      {/*<UserContext.Provider value={{users:[{name:'camilo',email:'ca.rincon97@gmail.com',password:'secret',balance:100}]}}>*/}
+    <HashRouter>
+     <div className="container" style={{padding: "20px"}}>
+      <NavBar/>
+      <UserContext.Provider value={{users:[{name:'camilo',email:'ca.rincon97@gmail.com',password:'secret',balance:100}]}}>*
         
         
-          <Route path="/" exact component={Home} />
+         <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
           <Route path="/deposit/" component={Deposit} />
@@ -14,11 +20,11 @@ function Spa() {
           <Route path="/balance/" component={Balance} />
           <Route path="/alldata/" component={AllData} />
              
-        </NavBar>  
-      </div>             
-       {/*</UserContext.Provider> */} 
+          </UserContext.Provider> 
+      </div>           
+       
         
-    {/*</HashRouter>*/}
+    </HashRouter>
       );
 }
 
